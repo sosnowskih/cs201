558db55b6c8b62d8db3b5e9efce85efb18c2dfd9
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-08-30 10:17:27 -0800

    modified the strings to clean up the output

--- labs/lab001/main.cpp
+++ labs/lab001/main.cpp
@@ -1,23 +1,22 @@
 //Program: Hello, World
 //Auther: Henryk Sosnowski
 //Date: 8/30/2019
-//Description: This program demonstrates basic C++ principles...
-//
-//
+//Description: This program demonstrates basic C++ principles by printing strings to the terminal and performing basic arithmatic
 
 #include<iostream>
 
 int main(int argc, char** argv)
 {
 	std::cout << "Hello, world!\n";
-	std::cout << "New line.\n";
+	std::cout << "Now let's do some basic math:\n";
 
-	int a = 67;
-	int b = 32;
+	int a = 42;
+	int b = 99;
 	int c = a * b;
 
 	std::cout << "a * b = " 
 			  << a << " * " << b << " = " << c 
-			  << ".\n";
+			  << ".\n\n";
+
 	return 0;
 }
