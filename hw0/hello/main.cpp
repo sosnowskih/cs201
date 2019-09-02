3c9e342f50c26725b2a9da1455029c4fefc87603
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-02 12:58:51 -0800

    added a program description and conditions in main for general practice

--- hw0/hello/main.cpp
+++ hw0/hello/main.cpp
@@ -1,14 +1,18 @@
-//
+//Program: Hello, World
+//Author: Henryk Sosnowski
+//Date: 9/02/2019
+//Description: This program demonstrates basic understanding of C++ by printing a string to the console five times.
 
-#include <iostream>
+#include<iostream>
 
-int main()
+int main(int argc, char** argv)
 {
 	int n = 0;
 	while (n < 5)
 	{
-		std::cout << "Hello, world!\n";
+		std::cout << "Hello, world\n";
 		n++;
 	}
+
 	return 0;
 }
