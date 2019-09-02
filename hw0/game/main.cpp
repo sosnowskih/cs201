b4edc28bc8889c25e8ca8a451de57b12e9c55fdc
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-02 15:16:51 -0800

    added the program, game, under hw0

---
+++ hw0/game/main.cpp
@@ -0,0 +1,15 @@
+//Program: Game
+//Author: Henryk Sosnowski
+//Date: 9/02/2019
+//Description: This program demonstrates basic understanding of C++ by printing a game description to the console.
+
+#include<iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Final Fantasy XIV\n\n"
+				 "FFXIV is an online RPG where you play as a charity worker who travels the world to solve everyone's menial problems.\n"
+				 "Along the way you are confronted by a team of ghosts who attack you with imagination.  A crystal was involved.\n" 
+				 "You can overcome these trials with a strict DPS rotation and thousands of flash drives.\n";
+	return 0;
+}
