0d15c176f7c2cde9de14168eeb3cc8f995b44993
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-02 14:50:50 -0800

    added the program, poem, under hw0

---
+++ hw0/poem/main.cpp
@@ -0,0 +1,18 @@
+//Program: Poem
+//Author: Henryk Sosnowski
+//Date: 9/02/2019
+//Description: This program demonstrates basic understanding of C++ by printing a terrible poem to the console.
+
+#include<iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "A Poem\n"
+			   	 "by Henryk Sosnowski\n\n"
+				 "Roses are red.\n"
+				 "Violets are blue.\n"
+				 "I don't write poetry,\n"
+				 "but I guess I'll have to.\n";
+
+	return 0;
+}
