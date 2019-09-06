ded808d0fb3467f79010cfa684bdab2e65728b6c
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-06 09:48:45 -0800

    added the program lab003-2

---
+++ labs/lab003-2/lab003-2.cpp
@@ -0,0 +1,25 @@
+//Program: lab003-3
+//Author: Henryk Sosnowski
+//Date 9/6/2019
+//Description: This program prints a 60x10 box of asterisks using nested while loops.
+
+#include<iostream>
+
+int main()
+{
+	int x = 0;
+	int y = 0;
+
+	while (y < 10)
+	{
+		while (x < 60)
+		{
+			std::cout << "*";
+			x++;
+		}
+		x = 0;
+		std::cout << std::endl;
+		y++;
+	}
+	return 0;
+}
