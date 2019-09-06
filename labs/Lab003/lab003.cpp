d70e346c24ed7a256887e51f019e22c6d2ae7332
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-06 09:40:01 -0800

    added lab003 program

---
+++ labs/Lab003/lab003.cpp
@@ -0,0 +1,17 @@
+//Author: Henryk Sosnowski
+//Class: CS201 Lab003
+//Date 9/6/2019
+//Description: This program prints 1-20 using a while loop
+
+#include<iostream>
+
+int main()
+{
+	int n = 1;
+	while (n < 21)
+	{
+		std::cout << n << std::endl;
+		n++;
+	}
+	return 0;
+}
