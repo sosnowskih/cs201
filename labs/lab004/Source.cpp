5602b836b063fe8308f6df2f186bc3182ef7bcca
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-11 09:49:31 -0800

    added fizz function

--- labs/lab004/Source.cpp
+++ labs/lab004/Source.cpp
@@ -1,11 +1,29 @@
 //Program: lab004
 //Author: Henryk Sosnowski
 //Date 9/11/2019
-//Description: 
+//Description: This program prints 1-100, replacing multiples of 3 with the word "Fizz"
 
 #include<iostream>
 
+void fizz()
+{
+	std::cout << "hi";
+	for (int i = 0; i < 100; ++i)
+	{
+		std::cout << "hi";
+		if (i % 3 == 0)
+		{
+			std::cout << "Fizz\n";
+		}
+		else
+		{
+			std::cout << i << std::endl;
+		}
+	}
+}
+
 int main()
 {
+	fizz;
 	return 0;
 }
