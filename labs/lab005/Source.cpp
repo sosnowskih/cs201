db263974fcae2ddb651a5305a2b0dbe4e506b86b
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-13 09:35:18 -0800

    added lab005

---
+++ labs/lab005/Source.cpp
@@ -0,0 +1,11 @@
+//Program: lab005
+//Author: Henryk Sosnowski
+//Date: 9/13/2019
+//Description: This program asks the user to input the size of a diamond, then prints it to the console.
+
+#include<iostream>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
