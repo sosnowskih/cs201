b998fbc10f0803962fbb920c21bdd410733deb6a
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-13 10:20:27 -0800

    added switcho and switchif functions

--- labs/lab005/Source.cpp
+++ labs/lab005/Source.cpp
@@ -5,7 +5,86 @@
 
 #include<iostream>
 
+void switcho()
+{
+	int input = 0;
+
+	int repeat = 1;
+	while (repeat == 1)
+	{
+		std::cout << "Enter a number to select an option: \n"
+			<< "1. Asleep\n"
+			<< "2. Awake\n"
+			<< "3. Nonplussed\n"
+			<< "4. Exit\n";
+		std::cin >> input;
+		std::cout << std::endl;
+
+		switch (input)
+		{
+		case 1:
+			std::cout << "UwU\n\n";
+			break;
+		case 2:
+			std::cout << "OwO\n\n";
+			break;
+		case 3:
+			std::cout << "`-`\n\n";
+			break;
+		case 4:
+			repeat = 0;
+			break;
+		default:
+			std::cout << "That's not an option!\n\n";
+			break;
+		}
+	}
+
+	std::cout << std::endl;
+}
+
+void switchif()
+{
+	int input = 0;
+
+	int repeat = 1;
+	while (repeat == 1)
+	{
+		std::cout << "Enter a number to select an option: \n"
+			<< "1. Asleep\n"
+			<< "2. Awake\n"
+			<< "3. Nonplussed\n"
+			<< "4. Exit\n";
+		std::cin >> input;
+		std::cout << std::endl;
+
+		if (input == 1)
+		{
+			std::cout << "UwU\n\n";
+		}
+		else if (input == 2)
+		{
+			std::cout << "OwO\n\n";
+		}
+		else if (input == 3)
+		{
+			std::cout << "`-`\n\n";
+		}
+		else if (input == 4)
+		{
+			repeat = 0;
+		}
+		else
+		{
+			std::cout << "That's not an option!\n\n";
+		}
+	}
+
+	std::cout << std::endl;
+}
+
 int main(int argc, char** argv)
 {
+	switchif();
 	return 0;
 }
