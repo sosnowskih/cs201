8bc2ac80409383395ec529594d599af52d3789c1
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-15 15:44:16 -0800

    added grid project

---
+++ hw1/grid/grid.cpp
@@ -0,0 +1,11 @@
+//Program: grid
+//Author: Henryk Sosnowski
+//Date: 9/15/2019
+//Description: This program displays a grid, asks the user for a grid location, then prints the grid again with an 'X' in the specified cell.
+
+#include<iostream>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
