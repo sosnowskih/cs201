8023d36d389513d3738b673c8f7deb4daa4533df
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-15 15:08:40 -0800

    added dotcross project

---
+++ hw1/dotcross/dotcross.cpp
@@ -0,0 +1,18 @@
+//Program: dotcross
+//Author: Henryk Sosnowski
+//Date: 9/15/2019
+//Description: This program asks the user for the x, y, and z values for two vectors, then calculates the dot and cross product.
+
+#include<iostream>
+
+int main(int argc, char** argv)
+{
+	int ax = 0;
+	int ay = 0;
+	int az = 0;
+	int bx = 0;
+	int by = 0;
+	int bz = 0;
+
+	return 0;
+}
