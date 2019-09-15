263975686febdf03fbc494a7427ac688f7e6dcf2
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-15 15:21:01 -0800

    added uinput function and all uses

--- hw1/dotcross/dotcross.cpp
+++ hw1/dotcross/dotcross.cpp
@@ -4,15 +4,33 @@
 //Description: This program asks the user for the x, y, and z values for two vectors, then calculates the dot and cross product.
 
 #include<iostream>
+#include<iomanip>
+
+//asks and returns user a vector coordinate value while displaying the vector name and coordinate name parameters
+double uinput(std::string vector, std::string coord)
+{
+	double input = 0;
+	std::cout << "For vector " << vector << ", enter " << coord << ": ";
+	std::cin >> input;
+	return input;
+}
 
 int main(int argc, char** argv)
 {
-	int ax = 0;
-	int ay = 0;
-	int az = 0;
-	int bx = 0;
-	int by = 0;
-	int bz = 0;
+	double ax = 0;
+	double ay = 0;
+	double az = 0;
+	double bx = 0;
+	double by = 0;
+	double bz = 0;
+
+	std::cout << "This program will calculate the dot and cross products of two vectors, A and B.\n";
+	ax = uinput("A", "x");
+	ay = uinput("A", "y");
+	az = uinput("A", "z");
+	bx = uinput("B", "x");
+	by = uinput("B", "y");
+	bz = uinput("B", "z");
 
 	return 0;
 }
