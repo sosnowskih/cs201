009eb0ae42a84de5822641fda8cd4f85b12e1736
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-16 10:33:52 -0800

    added name gathering loop under main

--- hw2/names/names.cpp
+++ hw2/names/names.cpp
@@ -4,9 +4,20 @@
 //Description: 
 
 #include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
 
 int main(int argc, char** argv)
 {
+	std::vector<std::string> names;
+	for (int i = 0; i < 3; i++)
+	{
+		std::string name;
+		std::cout << "Please enter a name: ";
+		std::getline(std::cin, name);
+		names.push_back(name);
+	}
 
 	return 0;
 }
