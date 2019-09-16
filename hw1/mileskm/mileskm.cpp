b0664ee3530b7aeea043fa634f01dbe7c88c5919
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-15 16:13:11 -0800

    added user input and unit conversion

--- hw1/mileskm/mileskm.cpp
+++ hw1/mileskm/mileskm.cpp
@@ -4,10 +4,22 @@
 //Description: This program converts a user value from miles to kilometers.
 
 #include<iostream>
+#include<iomanip>
 
 int main(int argc, char** argv)
 {
+	double miles = 0;
+	std::string plural = "miles";
 
+	std::cout << "This program will convert miles to kilometers.\n"
+		<< "Enter a number of miles: ";
+	std::cin >> miles;
 
-	return 0;
+	if (miles == 1)
+	{
+		plural = "mile";
+	}
+	std::cout << miles << " " << plural << " = " << std::setprecision(5) << 1.609 * miles << " km\n";
+	
+ 	return 0;
 }
