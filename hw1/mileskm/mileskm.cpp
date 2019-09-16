269239d0aa496d6b00708af8074637552a6c0a46
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-15 16:06:16 -0800

    added description and main

---
+++ hw1/mileskm/mileskm.cpp
@@ -0,0 +1,13 @@
+//Program: mileskm
+//Author: Henryk Sosnowski
+//Date: 9/15/2019
+//Description: This program converts a user value from miles to kilometers.
+
+#include<iostream>
+
+int main(int argc, char** argv)
+{
+
+
+	return 0;
+}
