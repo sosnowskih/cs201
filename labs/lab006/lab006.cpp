014bf9f409bf46ece714452d062071029e9978cf
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-16 10:01:27 -0800

    added required function

--- labs/lab006/lab006.cpp
+++ labs/lab006/lab006.cpp
@@ -5,8 +5,31 @@
 
 #include<iostream>
 
+void printStringLength(std::string userString)
+{
+	std::cout << "'" << userString << "' is " << userString.size() << " characters long.\n";
+}
+
+void requiredFunction()
+{
+	while (true)
+	{
+		std::string inputString;
+
+		std::cout << "Enter a string to get its length (0 to quit): ";
+		std::cin >> inputString;
+
+		if (inputString == "0")
+		{
+			break;
+		}
+
+		printStringLength(inputString);
+	}
+}
+
 int main(int argc, char** argv)
 {
-
+	requiredFunction();
 	return 0;
 }
