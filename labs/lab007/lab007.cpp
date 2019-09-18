137ec94a846c47ed17b854fff65d203905ae65e5
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-18 10:09:29 -0800

    added varyInt

--- labs/lab007/lab007.cpp
+++ labs/lab007/lab007.cpp
@@ -4,9 +4,21 @@
 //Description: 
 
 #include<iostream>
+#include<vector>
 #include"intio.hpp"
 
+void threeInt();
+
+void varyInt();
+
 int main()
+{
+	varyInt();
+
+	return 0;
+}
+
+void threeInt()
 {
 	int firstInt = 0;
 	int secondInt = 0;
@@ -24,6 +36,29 @@ int main()
 	std::cout << std::endl;
 	putInt(thirdInt, 10);
 	std::cout << std::endl;
+}
 
-	return 0;
+void varyInt()
+{
+	std::vector<int> userInts;
+	int input = 1;
+	int counter = 1;
+	int sum = 0;
+	std::cout << "Please enter integers (0 to end): ";
+	
+	while (input != 0)
+	{
+		input = getInt();
+		userInts.push_back(input);
+		++counter;
+	}
+
+	for (int n = 0; n < userInts.size()-1; n++)
+	{
+		putInt(userInts[n], 14);
+		std::cout << std::endl;
+		sum += userInts[n];
+	}
+	std::cout << "Sum = ";
+	putInt(sum, 8);
 }
