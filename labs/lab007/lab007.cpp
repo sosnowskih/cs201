cd1632fe4afd028e3da574ec108e4b07493464dd
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-18 09:48:09 -0800

    finished required

--- labs/lab007/lab007.cpp
+++ labs/lab007/lab007.cpp
@@ -3,11 +3,27 @@
 //Date: 9/18/2019
 //Description: 
 
-#include<istream>
+#include<iostream>
+#include"intio.hpp"
 
 int main()
 {
+	int firstInt = 0;
+	int secondInt = 0;
+	int thirdInt = 0;
 
+	std::cout << "Please enter three integers: ";
+	firstInt = getInt();
+	secondInt = getInt();
+	thirdInt = getInt();
+	std::cout << std::endl;
+
+	putInt(firstInt, 10);
+	std::cout << std::endl;
+	putInt(secondInt, 10);
+	std::cout << std::endl;
+	putInt(thirdInt, 10);
+	std::cout << std::endl;
 
 	return 0;
 }
