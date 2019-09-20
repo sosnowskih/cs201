be28194065804cba90cf015720d2ed4fc0301561
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-20 11:06:55 -0800

    added ListChange function and changed name vector to include singular names

--- hw2/money/money.cpp
+++ hw2/money/money.cpp
@@ -9,28 +9,50 @@
 #include<string>
 #include<algorithm>
 
-void InputChange(std::vector<int> & change);
+void InputChange(std::vector<int> & change, std::vector<std::string>& name);
+
+void ListChange(std::vector<int>& change, std::vector<std::string>& name);
 
 int main()
 {
 	std::vector<int> change;
+	std::vector<std::string> name = { "pennies", "nickels", "dimes", "quarters", "one-dollar coins" , "penny", "nickel", "dime", "quarter", "one-dollar coin"};
 
 	std::cout << "This program will count your change.\n";
 
-	InputChange(change);
+	InputChange(change, name);
+
+	std::cout << std::endl;
+
+	ListChange(change, name);
 
 	return 0;
 }
 
 //Asks the user for amount of different types of change and saves them to the change vector
-void InputChange(std::vector<int> & change)
+void InputChange(std::vector<int> & change, std::vector<std::string> & name)
 {
-	std::vector<std::string> name = {"pennies", "nickels", "dimes", "quarters", "one-dollar coins"};
-	for (int i = 0; i < name.size(); i++)
+	for (int i = 0; i < 5; i++)
 	{
 		std::cout << "How many " << name[i] << " do you have? ";
 		int input = 0;
 		std::cin >> input;
 		change.push_back(input);
+	}
+}
+
+//Lists the change from the values in the change vector
+void ListChange(std::vector<int>& change, std::vector<std::string>& name)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		if (change[i] == 1)
+		{
+			std::cout << "You have " << change[i] << " " << name[i + 5] << ".\n";
+		}
+		else
+		{
+			std::cout << "You have " << change[i] << " " << name[i] << ".\n";
+		}
 	}
 }
