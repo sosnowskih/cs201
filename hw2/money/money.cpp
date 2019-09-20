9c869d11d3c61cf3377d00417ffb3440b11ff172
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-20 10:36:53 -0800

    added money.cpp

---
+++ hw2/money/money.cpp
@@ -0,0 +1,20 @@
+/*Program: money
+*Author: Henryk Sosnowski
+*Date: 9/20/2019
+*Description: 
+*/
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+
+
+int main()
+{
+	std::cout << "This program will count your change.\n";
+
+
+	return 0;
+}
