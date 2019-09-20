5004244e616af69cdf46fbffce3498b9d7e0d9c2
Henryk Sosnowski <hwsosnowski@CEM-SOECAL-03>
2019-09-20 10:44:05 -0800

    added InputChange function

--- hw2/money/money.cpp
+++ hw2/money/money.cpp
@@ -9,12 +9,26 @@
 #include<string>
 #include<algorithm>
 
-
+int InputChange(std::string name);
 
 int main()
 {
-	std::cout << "This program will count your change.\n";
+	std::vector<int> change;
 
+	std::cout << "This program will count your change.\n";
+	change.push_back(InputChange("pennies"));
+
+
+	std::cout << change[0];
 
 	return 0;
+}
+
+//Asks for and returns user input based on the given string parameter
+int InputChange(std::string name)
+{
+	std::cout << "How many " << name << " do you have? ";
+	int n;
+	std::cin >> n;
+	return n;
 }
