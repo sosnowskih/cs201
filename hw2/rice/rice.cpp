aef05a312b81a213ca5fd205e57cb04f372988d7
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-22 18:21:45 -0800

    added function countRice

--- hw2/rice/rice.cpp
+++ hw2/rice/rice.cpp
@@ -5,10 +5,33 @@
 */
 
 #include<iostream>
+#include<vector>
+
+void countRice(int goal, int& squareNumber, int& squareValue, int& totalGrains);
 
 int main()
 {
+	int squareNumber = 1;
+	int squareValue = 1;
+	int totalGrains = 1;
 
+	countRice(1000, squareNumber, squareValue, totalGrains);
+	countRice(1000000, squareNumber, squareValue, totalGrains);
+	countRice(1000000000, squareNumber, squareValue, totalGrains);
 
 	return 0;
+}
+
+
+void countRice(int goal, int& squareNumber, int& squareValue, int& totalGrains)
+{
+	while (totalGrains < goal)
+	{
+		std::cout << "Square " << squareNumber << ": " << totalGrains << " grains of rice\n";
+		squareValue = squareValue * 2;
+		totalGrains = totalGrains + squareValue;
+		squareNumber++;
+	}
+	std::cout << "Square " << squareNumber << ": " << totalGrains << " grains of rice\n"
+		 	  << squareNumber << " squares are required for " << goal << " grains of rice.\n\n";
 }
