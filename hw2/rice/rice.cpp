9c38a6ae7cde4a18a32d9b7a011cdff1bd378fd2
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-22 18:40:37 -0800

    added calculation for type limit and results

--- hw2/rice/rice.cpp
+++ hw2/rice/rice.cpp
@@ -1,29 +1,42 @@
 /*Program: rice
 *Author: Henryk Sosnowski
 *Date: 9/22/2019
-*Description: 
+*Description: This program calculates the number of grains of rice the inventor would be owed
+			  and prints the number of squares needed for one thousand, million, and billion grains.
 */
 
 #include<iostream>
-#include<vector>
 
-void countRice(int goal, int& squareNumber, int& squareValue, int& totalGrains);
+void countRice(int goal, int& squareNumber, double& squareValue, double& totalGrains);
 
 int main()
 {
 	int squareNumber = 1;
-	int squareValue = 1;
-	int totalGrains = 1;
+	double squareValue = 1;
+	double totalGrains = 1;
 
 	countRice(1000, squareNumber, squareValue, totalGrains);
 	countRice(1000000, squareNumber, squareValue, totalGrains);
 	countRice(1000000000, squareNumber, squareValue, totalGrains);
 
+	std::cout << "31 squares can be calculated with ints, but all 64 squares can be approximated with doubles.\n";
+
+	/*
+	Calculations for variable type limit
+	while (squareNumber <= 64)
+	{
+		std::cout << "Square " << squareNumber << ": " << totalGrains << " grains of rice\n";
+		squareValue = squareValue * 2;
+		totalGrains = totalGrains + squareValue;
+		squareNumber++;
+	}
+	*/
+
 	return 0;
 }
 
-
-void countRice(int goal, int& squareNumber, int& squareValue, int& totalGrains)
+//This starts or continues calculation of rice grains per square until the set goal
+void countRice(int goal, int& squareNumber, double& squareValue, double& totalGrains)
 {
 	while (totalGrains < goal)
 	{
