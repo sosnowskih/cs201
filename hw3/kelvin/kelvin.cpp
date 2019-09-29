e23bb9fa19113a90d3744db6001226e54b705d8b
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-29 13:40:42 -0800

    fixed given code errors

--- hw3/kelvin/kelvin.cpp
+++ hw3/kelvin/kelvin.cpp
@@ -4,11 +4,26 @@
 *Description: This program converts a Celsius degree value to Kelvin.
 */
 
+#include<iostream>
 
+//Converts Celsius to Kelvin
+double ctok(double c)
+{
+	//int k = c + 273.15;  int will truncate values less than 1
+	double k = c + 273.15;
+	//return int;  You must return a value or variable, not a type
+	return k;
+}
 
 int main()
 {
-
+	double c = 0;
+	//cin >> d;   c was declared, not d
+	std::cin >> c;
+	//double k = ctok("c");  This passes a string containing "c", not the variable c
+	double k = ctok(c);
+	//Cout << k << '\n';   cout is case-sensitive 
+	std::cout << k << '\n';
 
 	return 0;
 }
