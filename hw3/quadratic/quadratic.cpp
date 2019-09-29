4cec68454c775237781d57ca6610023870923bc7
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-29 12:53:50 -0800

    added quadratic

---
+++ hw3/quadratic/quadratic.cpp
@@ -0,0 +1,16 @@
+/*Program: quadratic
+*Author: Henryk Sosnowski
+*Date: 9/29/2019
+*Description: This program computes the quadratic equation for the user, while checking for real roots.
+*/
+
+#include<iostream>
+
+int main()
+{
+	double a, b, c = 0;
+
+
+
+	return 0;
+}
