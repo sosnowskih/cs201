2deec8a6d63f64deba56afe0052506900d6ff4cc
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-29 13:21:11 -0800

    added calculation for complex roots

--- hw3/quadratic/quadratic.cpp
+++ hw3/quadratic/quadratic.cpp
@@ -6,6 +6,7 @@
 
 #include<iostream>
 #include<math.h>
+#include<cmath>
 
 int main()
 {
@@ -21,11 +22,13 @@ int main()
 
 	if (b * b - 4 * a * c < 0)
 	{
-		std::cout << "This equation has no real roots.\n";
+		std::cout << "This equation has no real roots.\n"
+			<< "The complex roots are " << (-b) / (2 * a) << " + " << sqrt(abs(b * b - 4 * a * c))/(2*a)
+			<< "i and " << (-b) / (2 * a) << " - " << sqrt(abs(b * b - 4 * a * c))/(2*a) << "i";
 	}
 	else
 	{
-		std::cout << "The roots are: " << (-b + sqrt(b * b - 4 * a * c)) / (2 * a) 
+		std::cout << "The roots are " << (-b + sqrt(b * b - 4 * a * c)) / (2 * a) 
 					<< " and " << (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
 	}
 	return 0;
