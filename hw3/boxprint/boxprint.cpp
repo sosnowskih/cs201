5e62bf3919716494be90d41bfcaeb3d21f594a8e
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-29 14:31:57 -0800

    added a check for negative size input

--- hw3/boxprint/boxprint.cpp
+++ hw3/boxprint/boxprint.cpp
@@ -1,6 +1,6 @@
 /*Program: boxprint
 *Author: Henryk Sosnowski
-*Date: 9/23/2019
+*Date: 9/29/2019
 *Description: This program prints a box of asterisks with a border thickness and contained string input by the user.
 */
 
@@ -22,11 +22,17 @@ int main()
 		std::cout << "Enter an integer (0 to quit): ";
 		std::cin >> size;
 
-		boxer(line, size);
-
 		if (size == 0)
 		{
 			break;
+		}
+		else if(size < 0)
+		{
+			std::cout << "Please enter a size greater than 0.\n";
+		}
+		else
+		{
+			boxer(line, size);
 		}
 	}
 
