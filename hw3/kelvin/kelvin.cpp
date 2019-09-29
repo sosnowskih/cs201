f062db4d26c6aa97852a0462f0351b6f641f356a
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-09-29 14:00:24 -0800

    added check for absolute zero

--- hw3/kelvin/kelvin.cpp
+++ hw3/kelvin/kelvin.cpp
@@ -11,6 +11,12 @@ double ctok(double c)
 {
 	//int k = c + 273.15;  int will truncate values less than 1
 	double k = c + 273.15;
+
+	if (k < 0)
+	{
+		return 0;
+	}
+
 	//return int;  You must return a value or variable, not a type
 	return k;
 }
@@ -20,6 +26,12 @@ int main()
 	double c = 0;
 	//cin >> d;   c was declared, not d
 	std::cin >> c;
+
+	if (c < -273.15)
+	{
+		std::cout << "\nInvalid temperature: below absolute 0\n";
+	}
+
 	//double k = ctok("c");  This passes a string containing "c", not the variable c
 	double k = ctok(c);
 	//Cout << k << '\n';   cout is case-sensitive 
