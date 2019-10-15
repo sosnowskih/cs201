08e99c403d1552a4c89e7634eb81fe61c2ba4146
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-10-15 14:34:03 -0800

    added StringToTokensWS() in tokenizer.cpp and a test for it in tokenizertest.cpp

--- hw4/tokenizer/tokenizer.cpp
+++ hw4/tokenizer/tokenizer.cpp
@@ -4,7 +4,7 @@
 #include<sstream>
 #include"tokenizer.hpp"
 
-
+//Reads a line of input from the user and returns false if the input is an empty line
 bool ReadLine(std::string& str)
 {
 	std::getline(std::cin, str);
@@ -15,11 +15,21 @@ bool ReadLine(std::string& str)
 	return true;
 }
 
-
+//Counts the number of tokens in a vector of strings and returns the total
 unsigned StringToTokensWS(std::vector<std::string>& tokens)
 {
-
-	return 0;
+	std::string token;
+	int total = 0;
+	for (int n = 0; n < tokens.size(); ++n)
+	{
+		token = tokens[n];
+		std::istringstream instream(token);
+		if (instream)
+		{
+			total++;
+		}
+	}
+	return total-1;
 }
 
 
--- hw4/tokenizer/tokenizertest.cpp
+++ hw4/tokenizer/tokenizertest.cpp
@@ -3,6 +3,10 @@ using std::cin;
 using std::cout;
 #include<string>
 using std::string;
+#include<sstream>
+using std::istringstream;
+#include<vector>
+using std::vector;
 #include"tokenizer.hpp"
 
 
@@ -11,7 +15,17 @@ int main()
 	cout << "Please enter a line: ";
 	string str; 
 	bool b = ReadLine(str);
-	cout << str << " " << b;
+	istringstream instream(str);
+	vector<string> tokens;
+
+	while (instream)
+	{
+		string tokenstring;
+		instream >> tokenstring;
+		tokens.push_back(tokenstring);
+	}
+
+	cout << StringToTokensWS(tokens);
 
 	return 0;
 }
