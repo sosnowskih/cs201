6c6170ed0246d1adca0ad1dff6efea868648a6f1
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-10-15 19:46:32 -0800

    added bulls-and-cows.cpp with userGuess()

---
+++ hw4/bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,95 @@
+//Program: bulls-and-cows.cpp
+//Author: Henryk Sosnowski
+//Date: 10/15/2019
+//Description: This program 
+
+
+
+#include<iostream>
+#include<vector>
+#include<sstream>
+#include<string>
+#include<math.h>
+
+void userGuess(std::vector<int> & guess);
+
+int main()
+{
+
+	std::vector<int> key { 1, 2, 3, 4 };
+	std::vector<int> guess;
+
+	while (true)
+	{
+		int bull = 0;
+		int cow = 0;
+		std::vector<int> bulls(4, 0);
+
+		userGuess(guess);
+
+		for (int n = 0; n < 4; ++n)
+		{
+			if (guess[n] == key[n])
+			{
+				++bull;
+				bulls[n] = 1;
+			}
+		}
+
+		for (int n = 0; n < 4; ++n)
+		{
+			if (bulls[n] == 0)
+			{
+				for (int i = 0; i < 4; ++i)
+				{
+					if (guess[n] == key[i])
+					{
+						++cow;
+					}
+				}
+			}
+		}
+
+		if (bull == 4)
+		{
+			break;
+		}
+		std::cout << "Incorrect.  There are " << bull << " bulls and " << cow << " cows.\n";
+
+		for (int i = 0; i < guess.size(); ++i)
+		{
+			std::cout << guess[i] << " ";
+		}
+	}
+
+	std::cout << "Correct, you won!\n";
+	return 0;
+}
+
+
+void userGuess(std::vector<int>& guess)
+{
+	std::cout << "Try to guess the four-digit number: ";
+	int intInput = 0;
+	std::string stringInput;
+
+	std::getline(std::cin, stringInput);
+	std::istringstream instream(stringInput);
+	instream >> intInput;
+
+	if (!instream || intInput < 1000 || intInput >= 10000)
+	{
+		while (intInput < 1000 || intInput >= 10000)
+		{
+			std::cout << intInput << "Error: Please enter a four-digit integer: ";
+			std::getline(std::cin, stringInput);
+			std::istringstream instream(stringInput);
+			instream >> intInput;
+		}
+	}
+
+	for (int n = 0; n < 4; ++n)
+	{
+		guess.push_back((int)stringInput[n]-48);
+	}
+}
