5f1a377e2296d5e22d63294f6412d5c9ca365f05
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-10-15 19:57:03 -0800

    added guessCheck() in bulls-and-cows.cpp

--- hw4/bulls-and-cows/bulls-and-cows.cpp
+++ hw4/bulls-and-cows/bulls-and-cows.cpp
@@ -13,53 +13,20 @@
 
 void userGuess(std::vector<int> & guess);
 
+void guessCheck(const std::vector<int>& guess, const std::vector<int>& key, bool & won);
+
+
 int main()
 {
-
 	std::vector<int> key { 1, 2, 3, 4 };
-	std::vector<int> guess;
+	bool won = false;
 
-	while (true)
+	while (!won)
 	{
-		int bull = 0;
-		int cow = 0;
-		std::vector<int> bulls(4, 0);
-
+		std::vector<int> guess;
 		userGuess(guess);
 
-		for (int n = 0; n < 4; ++n)
-		{
-			if (guess[n] == key[n])
-			{
-				++bull;
-				bulls[n] = 1;
-			}
-		}
-
-		for (int n = 0; n < 4; ++n)
-		{
-			if (bulls[n] == 0)
-			{
-				for (int i = 0; i < 4; ++i)
-				{
-					if (guess[n] == key[i])
-					{
-						++cow;
-					}
-				}
-			}
-		}
-
-		if (bull == 4)
-		{
-			break;
-		}
-		std::cout << "Incorrect.  There are " << bull << " bulls and " << cow << " cows.\n";
-
-		for (int i = 0; i < guess.size(); ++i)
-		{
-			std::cout << guess[i] << " ";
-		}
+		guessCheck(guess, key, won);
 	}
 
 	std::cout << "Correct, you won!\n";
@@ -91,5 +58,47 @@ void userGuess(std::vector<int>& guess)
 	for (int n = 0; n < 4; ++n)
 	{
 		guess.push_back((int)stringInput[n]-48);
+	}
+}
+
+void guessCheck(const std::vector<int>& guess, const std::vector<int>& key, bool &won) 
+{
+	int bull = 0;
+	int cow = 0;
+	std::vector<int> bulls(4, 0);
+
+	for (int n = 0; n < 4; ++n)
+	{
+		if (guess[n] == key[n])
+		{
+			++bull;
+			bulls[n] = 1;
+		}
+	}
+
+	for (int n = 0; n < 4; ++n)
+	{
+		if (bulls[n] == 0)
+		{
+			for (int i = 0; i < 4; ++i)
+			{
+				if (guess[n] == key[i])
+				{
+					++cow;
+				}
+			}
+		}
+	}
+
+	if (bull == 4)
+	{
+		won = true;
+		return;
+	}
+	std::cout << "Incorrect.  There are " << bull << " bulls and " << cow << " cows.\n";
+
+	for (int i = 0; i < guess.size(); ++i)
+	{
+		std::cout << guess[i] << " ";
 	}
 }
