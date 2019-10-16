fe0093e6b014e59907678cda22d79325b621141f
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-10-15 20:50:18 -0800

    added FifoPush() in fifo-lifo.cpp

--- hw4/fifo-lifo/fifo-lifo.cpp
+++ hw4/fifo-lifo/fifo-lifo.cpp
@@ -4,9 +4,16 @@
 #include"fifo-lifo.hpp"
 
 //First-In First-Out
+//Increases the size of the vector by one and adds the item to the front as index 0
 void FifoPush(std::vector<std::string>& container, const std::string& item)
 {
+	container.resize(container.size() + 1);
 
+	for (int i = 0; i < container.size()-1; ++i)
+	{
+		container[container.size() - i - 1] = container[container.size() - i - 2];
+	}
+	container[0] = item;
 }
 
 void FifoPop(std::vector<std::string>& container, const std::string& item)
--- hw4/fifo-lifo/fifo-lifomain.cpp
+++ hw4/fifo-lifo/fifo-lifomain.cpp
@@ -4,12 +4,27 @@
 //Description: This program 
 
 #include<iostream>
+using std::cout;
+using std::cin;
 #include<vector>
 #include<string>
 #include"fifo-lifo.hpp"
 
 int main()
 {
+	std::vector<std::string> container;
+	cout << "This program will demonstrate the concepts of First in First out and Last in First out.\n";
+
+	while (true)
+	{
+		cout << "Please select an option:\n"
+			<<"1. First in First out\n"
+			<<"2. Last in First out\n"
+			<<"3. Exit\n";
+		int choice = 0;
+		cin >> choice;
+
+	}
 
 	return 0;
 }
