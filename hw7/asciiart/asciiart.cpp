21a215f313ee3961d2a3a67bfc9efa6642a54009
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-11-24 18:34:51 -0900

    fixed formatting in main() in asciiart.cpp

--- hw7/asciiart/asciiart.cpp
+++ hw7/asciiart/asciiart.cpp
@@ -1,3 +1,9 @@
+//Program: asciiart.cpp
+//Author: Henryk Sosnowski
+//Date: 11/24/2019
+//Description: This program opens a PPM image file, converts it to ASCII art, and prints it to the console.
+
+
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -11,11 +17,10 @@ using std::endl;
 
 int main()
 {
-	
+	cout << "This program will convert parrot.ppm to ASCII art." << endl;
 	Image3 parrot(100, 100);
 	parrot.loadPPM("parrot.ppm");
 	parrot.printASCII(cout);
-	parrot.savePPM("outputp.ppm");
 
 	return 0;
 }
