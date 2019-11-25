bdc743bcfba940f4c719c6178fd3ed6afbd2a1af
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-11-24 19:59:48 -0900

    added cypher to caesar-cypher.cpp

--- hw7/caesar-cypher/caesar-cypher.cpp
+++ hw7/caesar-cypher/caesar-cypher.cpp
@@ -3,11 +3,71 @@
 //Date: 11/24/2019
 //Description: 
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+using std::vector;
+using std::string;
 
 int main() {
+	cout << "Caesar Cypher" << endl;
+
+	while (true) {
+		cout << "Enter a message to cypher (blank line to end): ";
+		string line = "";
+		string output = "";
+		getline(cin, line);
+		if (line == "end") break;
+
+		cout << "Enter an integer to use as a shift: ";
+		int shift = 0;
+		string shiftline;
+		getline(cin, shiftline);
+		std::istringstream instream(shiftline);
+		instream >> shift;
+		while (!instream)
+		{
+			cout << "Error: Please enter an integer: ";
+			getline(cin, shiftline);
+			std::istringstream instream(shiftline);
+			instream >> shift;
+		}
 
 
+		shift = shift % 26;
 
+		const char upper[] = "ABCDEFGHIJKLMNOPQURTUVWXYZABCDEFGHIJKLMNOPQURTUVWXYZABCDEFGHIJKLMNOPQURTUVWXYZ";
+		const char lower[] = "abcdefghijklmnopqurtuvwxyzabcdefghijklmnopqurtuvwxyzabcdefghijklmnopqurtuvwxyz";
+
+		for (auto i = 0; i < line.length(); i++)
+		{
+			if ((line.at(i) >= 'A') & (line.at(i) <= 'Z'))
+			{
+				char c = line.at(i);
+				int index = 0;
+				while (c != upper[index]) index++;
+
+				output.push_back((char)upper[index + shift + 26]);
+			}
+			else if ((line.at(i) >= 'a') & (line.at(i) <= 'z'))
+			{
+				char c = line.at(i);
+				int index = 0;
+				while (c != lower[index]) index++;
+
+				output.push_back((char)lower[index + shift + 26]);
+			}
+			else output.push_back(line.at(i));
+		}
+
+		cout << "Result: " << output << endl;
+	}
 
 	return 0;
 }
