d1c5fa1b7943d0fe42ad3da3384172b2a38a954b
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-12-08 21:38:19 -0900

    added Record and food items to shopping.cpp

--- hw8/shopping/shopping.cpp
+++ hw8/shopping/shopping.cpp
@@ -10,13 +10,39 @@
 #include <map>
 
 using std::cout;
+using std::cin;
 using std::endl;
 using std::string;
 using std::map;
 
 
-int main() {
+struct Record {
+	double unitPrice;
+	int units;
+};
 
+
+int main() {
+	cout << "This program simulates a grocery store." << endl;
+
+	map<string, Record> m;
+
+	while (true) {
+		cout << "Enter the number listed to add an item, any other key to view your cart:\n"
+			<< "1. Eggs - $5\n"
+			<< "2. Milk - $3\n"
+			<< "3. Bread - $4\n"
+			<< "4. Grapes - $2\n"
+			<< "5. Salmon - $10\n" << endl;
+
+		char input;
+		cin >> input;
+
+
+		
+
+
+	}
 
 	return 0;
 }
