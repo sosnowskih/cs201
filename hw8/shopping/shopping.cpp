e5bb5cbe66be4a21afaff689f7155af6658bd1e8
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-12-08 22:01:01 -0900

    added item option to shopping.cpp

--- hw8/shopping/shopping.cpp
+++ hw8/shopping/shopping.cpp
@@ -26,19 +26,44 @@ int main() {
 	cout << "This program simulates a grocery store." << endl;
 
 	map<string, Record> m;
+	std::vector<string> names {"Eggs", "Milk", "Break", "Grapes", "Salmon"};
 
+	//Main menu loop
 	while (true) {
-		cout << "Enter the number listed to add an item, any other key to view your cart:\n"
+		cout << "Items for sale:\n"
 			<< "1. Eggs - $5\n"
 			<< "2. Milk - $3\n"
 			<< "3. Bread - $4\n"
 			<< "4. Grapes - $2\n"
-			<< "5. Salmon - $10\n" << endl;
+			<< "5. Salmon - $10\n"
+			<< "6. View Cart\n"
+			<< "Select an option (0 to quit): ";
 
-		char input;
+		int input;
 		cin >> input;
 
+		//Quit option
+		if (input == 0) {
+			return 0;
+		}
 
+		//Adding item option
+		else if (input < 6) {
+			cout << "How much would you like to add? ";
+			int count;
+			cin >> count;
+			m[names[input]].units += count;
+		}
+
+		//Cart option
+		else if (input == 6) {
+			cout << "Your cart:\n";
+				for (int i = 0; i < 5; i++) cout << names[i] << " x" << m[names[i]].units << "\n";
+				cout << "Total: $" << 
+		}
+		else {
+			cout << "Input error" << endl;
+		}
 		
 
 
