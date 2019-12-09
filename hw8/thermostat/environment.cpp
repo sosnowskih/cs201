99787f0b3fb73ab68e844d8e89630c6b4dd70d0d
Henryk Sosnowski <54690256+sosnowskih@users.noreply.github.com>
2019-12-08 15:07:20 -0900

    added constructor and iteration in environment.hpp and environment.cpp

--- hw8/thermostat/environment.cpp
+++ hw8/thermostat/environment.cpp
@@ -1,1 +1,10 @@
 #include "environment.hpp"
+
+void Environment::iteration() {
+	if (_heater) {
+		_temp += 1;
+	}
+	else {
+		_temp -= 1;
+	}
+}
--- hw8/thermostat/environment.hpp
+++ hw8/thermostat/environment.hpp
@@ -1,5 +1,21 @@
 #ifndef ENVIRONMENT_HPP
 #define ENVIRONMENT_HPP
 
+class Environment {
+public:
+	//constructor
+	Environment(int t, bool h) :
+		_temp{ t }, _heater{ h }{}
+
+	//member functions
+	void iteration();
+
+
+//data members
+private:
+	int _temp = 0;
+	bool _heater = false;
+
+};
 
 #endif
